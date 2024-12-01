3b8696da55f47f0562934589c34e10923bb74297
DaceVil <vilcina.dace@gmail.com>
2024-12-02 00:48:15 +0200

    added TC x3

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -79,18 +79,19 @@ test('Test X2', async ({ page }) => {
   // Compare the actual text of the first product to the expected string
   expect(firstProductText.trim()).toBe('Sauce Labs Onesie');
 
+  // await page.waitForTimeout(1000);
   await page.click('.select_container');
 
   const filterMenu2 = await page.isVisible('.product_sort_container');
   expect(filterMenu).toBe(true);
-  //sort by low to high
+  //sort by hogh to low
   const dropdownSelector2 = '.product_sort_container';  // Assuming this is the select element
 
   await page.waitForSelector(dropdownSelector2, { state: 'visible' });
   // Choose the option by value or label (depending on what the dropdown is using)
-  await page.selectOption(dropdownSelector, { value: 'hilo' }); // Use the label, value, or index
+  await page.selectOption(dropdownSelector2, { value: 'hilo' }); // Use the label, value, or index
 
-  //first item should be Sauce Labs Onesie
+  //first item should be Sauce Labs Fleece Jacket
   const firstProduct2 = await page.locator('.inventory_item_name').first();
 
   // Get the text content of the first product
@@ -98,9 +99,86 @@ test('Test X2', async ({ page }) => {
 
   // Compare the actual text of the first product to the expected string
   expect(firstProductText2.trim()).toBe('Sauce Labs Fleece Jacket');
+  // await page.waitForTimeout(1000);
+  //sort by A to Z
+  const dropdownSelector3 = '.product_sort_container';  // Assuming this is the select element
+
+  await page.waitForSelector(dropdownSelector3, { state: 'visible' });
+  // Choose the option by value or label (depending on what the dropdown is using)
+  await page.selectOption(dropdownSelector3, { value: 'az' }); // Use the label, value, or index
+
+  //first item should be Sauce Labs Backpack
+  const firstProduct3 = await page.locator('.inventory_item_name').first();
+
+  // Get the text content of the first product
+  const firstProductText3 = await firstProduct.textContent();
+
+  // Compare the actual text of the first product to the expected string
+  expect(firstProductText3.trim()).toBe('Sauce Labs Backpack');
+  // await page.waitForTimeout(1000);
+  //sort by Z to A
+  const dropdownSelector4 = '.product_sort_container';  // Assuming this is the select element
+
+  await page.waitForSelector(dropdownSelector4, { state: 'visible' });
+  // Choose the option by value or label (depending on what the dropdown is using)
+  await page.selectOption(dropdownSelector4, { value: 'za' }); // Use the label, value, or index
+
+  //first item should be Sauce Labs Backpack
+  const firstProduct4 = await page.locator('.inventory_item_name').first();
+
+  // Get the text content of the first product
+  const firstProductText4 = await firstProduct.textContent();
+
+  // Compare the actual text of the first product to the expected string
+  expect(firstProductText4.trim()).toBe('Test.allTheThings() T-Shirt (Red)');
+  // await page.waitForTimeout(1000);
 
 });
 
+
+test('Test X3 add to chart and go to checkout', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  //input[@id='user-name']
+  await page.locator("//input[@id='user-name']").type('standard_user');
+  //input password
+  await page.locator("//input[@id='password']").type('secret_sauce');
+  //press login button
+  await page.locator("//input[@id='login-button']").click();
+  //find item Sauce Labs Bike Light add to chart and go to checkout
+  //const productLocator = page.locator('.inventory_item_name').first();
+  // Click on the first product
+
+  await page.locator('text=Sauce Labs Bike Light').click();
+
+  // Wait for the price element to be visible
+  await page.waitForSelector('.inventory_item_price', { state: 'visible' });
+
+  // // Get the price of the product
+  // const itemPrice = await page.locator('.inventory_item_price').textContent();
+  // expect(itemPrice.trim()).toBe('9.99');  // Check if the price is correct
+  // //click on filter icon
+  // await page.click('.select_container');
+
+  // const filterMenu = await page.isVisible('.product_sort_container');
+  // expect(filterMenu).toBe(true);
+  // //sort by low to high
+  // const dropdownSelector = '.product_sort_container';  // Assuming this is the select element
+
+  // await page.waitForSelector(dropdownSelector, { state: 'visible' });
+  // // Choose the option by value or label (depending on what the dropdown is using)
+  // await page.selectOption(dropdownSelector, { value: 'lohi' }); // Use the label, value, or index
+
+  // //first item should be Sauce Labs Onesie
+  // const firstProduct = await page.locator('.inventory_item_name').first();
+
+  // // Get the text content of the first product
+  // const firstProductText = await firstProduct.textContent();
+
+  // // Compare the actual text of the first product to the expected string
+  // expect(firstProductText.trim()).toBe('Sauce Labs Onesie');
+
+
+});
 
 test('hover over sign in', async ({ page }) => {
   await page.goto('https://danube-web.shop/');
